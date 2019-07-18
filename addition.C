f11d4de61112bd70f3bfd0d5077d9548d80c924a
prachidisha2311 <53028881+prachidisha2311@users.noreply.github.com>
2019-07-18 09:22:04 +0530

    Create addition.C

---
+++ addition.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+printf("Welcome to horizon 2k19");
+printf("Let do simple addition and substraction in c")
+int a,b,c;
+printf("Enter value of a:");
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+printf("The addition of two numbers is:%d",c);
+printf("The difference of two numbers is:%d",d);
+}
